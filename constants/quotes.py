2442412be496dd08a1240933754c74950b6423d5
Julius Decena <julius.decena3095@gmail.com>
2021-08-08 23:23:35 +0800

    added oath quotes

--- constants/quotes.py
+++ constants/quotes.py
@@ -11,6 +11,49 @@ UMBASAM_QUOTES = [
     "kung si ann mateo gagank sakin ayos lang..."
 ]
 
+UMBAMEAT_QUOTES = [
+    "HELLO TEAM?!",
+
+]
+
+UMBATANOD_QUOTES = [
+    "HEY KID... DO YOU WANNA CARAVAN?",
+    "HINDI ESSENTIAL ANG LUGAW MGA SER."
+]
+
+UMBARYOTA_QUOTES = [
+    "https://youtu.be/NTdjCgnLGsM",
+    "TANGINA MO BLUVER",
+    "TANGINA MO BLUVER",
+    "TANGINA MO BLUVER",
+    "TANGINA MO BLUVER",
+    "TANGINA MO BLUVER",
+    "TANGINA MO BLUVER",
+    "TANGINA MO BLUVER",
+    "TANGINA MO BLUVER",
+]
+
+UMBACHOKYY_QUOTES = [
+    "LEZGOOOOO",
+    "5, 6, 7, 8!",
+    "okay lang yun ggawa tayo bagong chismis",
+    "gorilla 8===========D Princess Sam",
+    "With extra service?"
+
+
+]
+
+UMBAFROST_QUOTES = [
+    "Ayuko sayangin yung braincells ko sa pag eenglish.",
+    "Oras na bang awayin ulit si Chokyy?"
+]
+
+UMBAOATH_GIFS = [
+    
+]
+
+UMBASAM_REMINDERS = []
+
 UMBASAM_CTA = [
     "PATAY KUNG PATAY!! JOIN CTA COMMS NOW!",
     "TIME FOR NEKS LEBEL PLAY! JOIN CTA COMMS NOW!",
--- modules/quotes.py
+++ modules/quotes.py
@@ -5,3 +5,16 @@ def generate_quote():
 
 def generate_cta():
     return random.sample(quotes.UMBASAM_CTA, 1)
+
+
+def generate_oath(oath):
+    if (oath == "meat"):
+        return random.sample(quotes.UMBAMEAT_QUOTES, 1)
+    elif (oath == "chokyy"):
+        return random.sample(quotes.UMBACHOKYY_QUOTES, 1)
+    elif (oath == "ryota"):
+        return random.sample(quotes.UMBARYOTA_QUOTES, 1)
+    elif (oath == "tanod" or oath == "morethin"):
+        return random.sample(quotes.UMBATANOD_QUOTES, 1)
+    elif (oath == "frost"):
+        return random.sample(quotes.UMBAFROST_QUOTES, 1)
