d208866673a102c8121f7ccce2f3c8654de46449
Julius Decena <julius.decena3095@gmail.com>
2021-08-08 15:18:25 +0800

    fixes for umbaguide channel message sender

--- modules/guide.py
+++ modules/guide.py
@@ -1,8 +1,8 @@
 import constants.strings as strings
 
 async def cdg(ctx):
-    await ctx.message.send("The Oathbreaker's [Corrupted Dungeon Guide]({0})".format(strings.URL_CORRUPTED))
+    await ctx.message.channel.send("The Oathbreaker's [Corrupted Dungeon Guide]({0})".format(strings.URL_CORRUPTED))
 
 
 async def pvp(ctx):
-    await ctx.message.send("The Oathbreaker's [MoreThin's PvP Guide]({0})".format(strings.URL_PVP))
+    await ctx.message.channel.send("The Oathbreaker's [MoreThin's PvP Guide]({0})".format(strings.URL_PVP))
